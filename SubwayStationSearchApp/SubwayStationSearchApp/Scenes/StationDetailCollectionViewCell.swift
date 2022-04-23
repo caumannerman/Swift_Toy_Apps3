a30f00bb2b2e467bb74b6fa91f0fb11a1c4dde93
Caumannerman <uigohiiming2@gmail.com>
2022-04-23 16:06:27 +0900

    지하철 상세 페이지에 CollectionViewController, ViewCell 추가

---
+++ SubwayStationSearchApp/SubwayStationSearchApp/Scenes/StationDetailCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  StationDetailCollectionViewCell.swift
+//  SubwayStationSearchApp
+//
+//  Created by 양준식 on 2022/04/23.
+//
+
+import UIKit
+import SnapKit
+
+
+final class StationDetailCollectionViewCell: UICollectionViewCell{
+    func setup(){
+        backgroundColor = .gray
+        
+    }
+}
--- SubwayStationSearchApp/SubwayStationSearchApp/Scenes/StationDetailViewController.swift
+++ SubwayStationSearchApp/SubwayStationSearchApp/Scenes/StationDetailViewController.swift
@@ -11,4 +11,40 @@ import UIKit
 
 final class StationDetailViewController: UIViewController {
     
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.estimatedItemSize = CGSize(width: view.frame.width - 32.0, height: 100.0)
+        layout.sectionInset = UIEdgeInsets(top: 16.0, left: 16.0, bottom: 16.0, right: 16.0)
+        layout.scrollDirection = .vertical
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(StationDetailCollectionViewCell.self, forCellWithReuseIdentifier: "StationDetailCollectinoViewCell")
+        
+        collectionView.dataSource = self
+        return collectionView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "왕십리"
+        view.addSubview(collectionView)
+        collectionView.snp.makeConstraints{
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
+extension StationDetailViewController: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+       guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "StationDetailCollectinoViewCell", for: indexPath) as? StationDetailCollectionViewCell else { return UICollectionViewCell()}
+        
+        cell.setup()
+        return cell
+    }
 }
