56c1a0396a4c6eab5a942ea2aab20b55686dad94
Caumannerman <uigohiiming2@gmail.com>
2022-04-23 15:36:59 +0900

    지하철 상세 페이지 추가, tableVIew의 didSelectRowAt메서드로 연결

---
+++ SubwayStationSearchApp/SubwayStationSearchApp/Scenes/StationDetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  StationDetailViewController.swift
+//  SubwayStationSearchApp
+//
+//  Created by 양준식 on 2022/04/23.
+//
+
+import SnapKit
+import UIKit
+
+
+final class StationDetailViewController: UIViewController {
+    
+}
--- SubwayStationSearchApp/SubwayStationSearchApp/Scenes/StationSearchViewController.swift
+++ SubwayStationSearchApp/SubwayStationSearchApp/Scenes/StationSearchViewController.swift
@@ -9,10 +9,13 @@ import UIKit
 import SnapKit
 
 class StationSearchViewController: UIViewController {
+    private var numberOfCells: Int = 0
     
     private lazy var tableView: UITableView = {
         let tableView = UITableView()
         tableView.dataSource = self
+        tableView.delegate = self
+        tableView.isHidden = true
         
         return tableView
     }()
@@ -34,6 +37,7 @@ class StationSearchViewController: UIViewController {
         searchController.searchBar.placeholder = "지하철 역을 입력해주세요."
         //화면 어두워지지 않도록 false 처리
         searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.delegate = self
         
         // UISearchController embedded
         navigationItem.searchController = searchController
@@ -47,16 +51,37 @@ class StationSearchViewController: UIViewController {
     }
 }
 
+extension StationSearchViewController: UISearchBarDelegate{
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        numberOfCells = 10
+        //지웠다 다시 그려줘야함 
+        tableView.reloadData()
+        tableView.isHidden = false
+    }
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        numberOfCells = 0
+        tableView.isHidden = true
+    }
+}
+
 
 extension StationSearchViewController: UITableViewDataSource{
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         cell.textLabel?.text = "\(indexPath.item)"
-        
+       
         return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return numberOfCells
+    }
+}
+
+
+extension StationSearchViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = StationDetailViewController()
+        navigationController?.pushViewController(vc, animated: true)
     }
 }
