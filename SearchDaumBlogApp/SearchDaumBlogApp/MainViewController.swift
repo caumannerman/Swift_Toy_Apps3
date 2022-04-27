ff183b666707c045190c5f8bce2c478321a2bd65
Caumannerman <uigohiiming2@gmail.com>
2022-04-27 17:39:52 +0900

    코드로 진행을 위해 storyboard삭제, MainViewController 생성, SceneDelegate 수정

---
+++ SearchDaumBlogApp/SearchDaumBlogApp/MainViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MainViewController.swift
+//  SearchDaumBlogApp
+//
+//  Created by 양준식 on 2022/04/27.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+final class MainViewController: UIViewController{
+    
+}
